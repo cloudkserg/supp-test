871bbc840f291fe105c3db029ded381da854b131
kirya <cloudkserg11@gmail.com>
2017-06-24 18:56:56 +0300

    fixed TokenService create token for user with user and company

--- app/Services/TokenService.php
+++ app/Services/TokenService.php
@@ -8,6 +8,9 @@
 
 namespace App\Services;
 
+use App\Company;
+use Auth;
+use App\User;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -21,10 +24,19 @@ class TokenService
 
     public function createToken($email, $password)
     {
+        $token = null;
         try {
-            if (!$token = JWTAuth::attempt(array('email' => $email, 'password' => $password, 'confirmed' => true))) {
+            if (!$user = Auth::attempt(['email' => $email, 'password' => $password, 'confirmed' => true])) {
                 throw new UnauthorizedHttpException("Email address / password do not match");
             }
+
+            /**
+             * @var User $user
+             */
+            $token = JWTAuth::fromUser($user, [
+                'id' => $user->id,
+                'company' => $user->company->title
+            ]);
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
             throw new HttpException("Unable to login");
