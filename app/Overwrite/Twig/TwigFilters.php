28517ec3e827d4bd2300093e98cbea85a257a83b
kirya <cloudkserg11@gmail.com>
2017-09-14 14:24:29 +0300

    add assets_path function to twig

--- app/Overwrite/Twig/TwigFilters.php
+++ app/Overwrite/Twig/TwigFilters.php
@@ -6,10 +6,16 @@ class TwigFilters extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('get_env', [$this, 'getEnv']),
+            new \Twig_SimpleFunction('assets_path', [$this, 'getAssetPath']),
         ];
     }
     public function getEnv($variable)
     {
         return env($variable);
+    }
+
+    public function assetPath($variable)
+    {
+        return resource_path('assets/' . $variable);
     }
 }
