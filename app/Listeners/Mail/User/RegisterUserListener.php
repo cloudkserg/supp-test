4387a917b570accf47bf9365ff5e1dbf5b521952
kirya <cloudkserg11@gmail.com>
2017-09-23 14:21:23 +0300

    add RegisterUserForAdmin mail

--- app/Listeners/Mail/User/RegisterUserListener.php
+++ app/Listeners/Mail/User/RegisterUserListener.php
@@ -5,6 +5,7 @@ namespace App\Listeners\Mail\User;
 use App\Demand\ResponseItem;
 use App\Events\RegisterUserEvent;
 use App\Mail\RegisterUser;
+use App\Mail\RegisterUserForAdmin;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -31,7 +32,7 @@ class RegisterUserListener implements ShouldQueue
         $user = $event->getUser();
         \Mail::to($user->email)->send(new RegisterUser($user));
 
-        \Mail::to(config('mail.adminEmails'))->send(new RegisterUser($user));
+        \Mail::to(config('mail.adminEmails'))->send(new RegisterUserForAdmin($user));
 
     }
 }
